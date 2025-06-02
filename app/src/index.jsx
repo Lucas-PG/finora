7658790c2f878a10b94e3f5d2e8cef19ec625baf
Lucas-PG <luscagape@gmail.com>
2025-06-02 10:13:56 -0300

    Adiciona ThemeProvider

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import App from "./App";
+import { ThemeProvider } from "./components/ThemeContext.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App />
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>,
 );
+
