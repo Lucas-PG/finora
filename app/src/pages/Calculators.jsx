6605ebc3ba663ec3c76a870c33e1d291b894d7c9
Lucas Gasperin <luscagape@gmail.com>
2025-06-04 06:47:14 -0300

    Adiciona Calculators

--- app/src/pages/Calculators.jsx
+++ app/src/pages/Calculators.jsx
@@ -1,71 +1,95 @@
-import { Link } from "react-router-dom";
-import Navbar from "../components/NavBar";
 import "../css/Calculators.css";
 
+import { FaCalculator } from "react-icons/fa";
+import HeroSection from "../components/HeroSection";
+import { Link } from "react-router-dom";
+import Navbar from "../components/NavBar";
+
 function Calculators() {
+  const title = "Calculadoras de Investimentos";
+  const subtitle =
+    "Use nossas ferramentas para calcular, planejar e prever seus investimentos";
+
   return (
     <>
       <Navbar />
-      <div className="calculators-container">
-        <div className="calculators-content">
-          <div className="calculators-title-div">
-            <h2>Calculadoras de Investimentos</h2>
-            <p className="calculators-subtitle">
-              Faça os cálculos necessários para planejar, simular ou prever seus
-              investimentos.
-            </p>
+      <HeroSection title={title} subtitle={subtitle} />
+      <div className="page-container">
+        <div className="page-content">
+          <div className="calculators-card-section">
+            <Link className="calculators-card" to="/compound-interest">
+              <div className="calculator-icon-section">
+                <div className="calculator-icon-around">
+                  <FaCalculator size={20} className="calculator-icon" />
+                </div>
+              </div>
+              <div className="calculator-text-section">
+                <h3>Juros Compostos</h3>
+                <span>
+                  O modelo mais comum em aplicações de renda fixa. Veja sua
+                  "bola de neve” formando.
+                </span>
+              </div>
+            </Link>
+
+            <Link className="calculators-card" to="/simple-interest">
+              <div className="calculator-icon-section">
+                <div className="calculator-icon-around">
+                  <FaCalculator size={20} className="calculator-icon" />
+                </div>
+              </div>
+              <div className="calculator-text-section">
+                <h3>Juros Simples</h3>
+                <span>
+                  Calcule os rendimentos de seus investimentos usando juros
+                  simples.
+                </span>
+              </div>
+            </Link>
+            <Link className="calculators-card" to="/first-million">
+              <div className="calculator-icon-section">
+                <div className="calculator-icon-around">
+                  <FaCalculator size={20} className="calculator-icon" />
+                </div>
+              </div>
+              <div className="calculator-text-section">
+                <h3>Primeiro Milhão</h3>
+                <span>
+                  Veja quanto tempo e quanto investir por mês para alcançar o
+                  seu primeiro milhão.
+                </span>
+              </div>
+            </Link>
+            <Link className="calculators-card" to="/asset-percentage">
+              <div className="calculator-icon-section">
+                <div className="calculator-icon-around">
+                  <FaCalculator size={20} className="calculator-icon" />
+                </div>
+              </div>
+              <div className="calculator-text-section">
+                <h3>Alocação de Portfolio</h3>
+                <span>
+                  Monte sua carteira ideal organizando seus ativos por tipo e
+                  perfil de investimento.
+                </span>
+              </div>
+            </Link>
           </div>
-
-          <div className="calculators-grid">
-            <div className="calculators-card">
-              <h3>Juros Compostos</h3>
-              <p>
-                Descubra quanto seus investimentos podem render com juros
-                compostos. Esse é o modelo mais comum em aplicações de renda
-                fixa.
-              </p>
-              <Link to="/CompoundInterest">Simular ›</Link>
-            </div>
-
-            <div className="calculators-card">
-              <h3>Juros Simples</h3>
-              <p>
-                Calcule os rendimentos de seus investimentos usando juros
-                simples. Ideal para entender ganhos em aplicações mais diretas
-                ou de curto prazo.
-              </p>
-              <Link to="/SimpleInterest">Simular ›</Link>
-            </div>
-
-            <div className="calculators-card">
-              <h3>Primeiro Milhão</h3>
-              <p>
-                Veja quanto tempo e quanto investir por mês para alcançar o seu
-                primeiro milhão.
-              </p>
-              <Link to="/FirstMillion">Simular ›</Link>
-            </div>
-
-            <div className="calculators-card">
-              <h3>Porcentagem de Ativos</h3>
-              <p>
-                Organize sua carteira de investimentos por tipo de ativo.
-                Calcule a distribuição ideal entre renda fixa, variável, fundos
-                e mais.
-              </p>
-              <Link to="/AssetPercentage">Simular ›</Link>
-            </div>
-          </div>
-
-          <div className="suggestion-section">
-            <div className="suggestion-content">
-              <h4>Não encontrou o que precisava?</h4>
-              <span>
-                Estamos sempre trabalhando para ajudar nossos usuários a tomar
-                as melhores decisões de investimentos. Que tal nos falar o que
-                te ajudaria?
-              </span>
-              <button className="suggest-btn">Sugerir Ferramenta</button>
+          <div className="suggest-tool-section">
+            <div className="suggest-tool-card">
+              <div className="suggest-title-section">
+                <h2>Não encontrou o que precisava?</h2>
+                <span>
+                  Estamos sempre trabalhando para ajudar nossos usuários a tomar
+                  as melhores decisões de investimentos. Que tal nos contar que
+                  tipo de ferramenta te ajudaria?
+                </span>
+              </div>
+              <div className="suggest-btn-section">
+                <button type="button" className="primary-btn suggest-btn">
+                  Sugerir Ferramenta
+                </button>
+              </div>
             </div>
           </div>
         </div>
